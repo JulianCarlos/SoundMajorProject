7729d88c32fc06676a03a97765d2a2b5532e53fb
JulianCarlos <88396363+JulianCarlos@users.noreply.github.com>
2024-04-04 21:25:25 +0200

    Added Creation and Apply Layer Method

--- Packages/com.tecmaid.spatialpathfinding/Editor/PathingManagerEditor.cs
+++ Packages/com.tecmaid.spatialpathfinding/Editor/PathingManagerEditor.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using Pathfinding.Helpers;
+using UnityEngine.PlayerLoop;
 
 namespace Pathfinding
 {
@@ -8,6 +10,15 @@ namespace Pathfinding
     public class PathingManagerEditor : Editor
     {
         public VisualTreeAsset VisualTree;
+        public PathingManager Manager;
+
+        public Button createLayersButton;
+        public Button applyLayersButton;
+
+        private void OnEnable()
+        {
+            Manager = (PathingManager)target;
+        }
 
         public override VisualElement CreateInspectorGUI()
         {
@@ -15,7 +26,41 @@ namespace Pathfinding
 
             VisualTree.CloneTree(root);
 
+            createLayersButton = root.Q<Button>("CreateLayersButton");
+            createLayersButton.RegisterCallback<ClickEvent>(GenerateLayers);
+
+            applyLayersButton = root.Q<Button>("ApplyLayersButton");
+            applyLayersButton.RegisterCallback<ClickEvent>(ApplyLayersToObjects);
+
             return root;
+        }
+
+        public void GenerateLayers(ClickEvent evt)
+        {
+            TagFactory.CreateLayer(Manager.AgentLayerName);
+            TagFactory.CreateLayer(Manager.VolumeLayerName);
+        }
+
+        public void ApplyLayersToObjects(ClickEvent evt)
+        {
+            if (TagFactory.LayerExists(Manager.AgentLayerName) && TagFactory.LayerExists(Manager.VolumeLayerName))
+            {
+                FlyingAgent[] agents = FindObjectsOfType<FlyingAgent>();
+                for (int i = 0; i < agents.Length; i++)
+                {
+                    agents[i].gameObject.layer = LayerMask.NameToLayer(Manager.AgentLayerName);
+                }
+
+                NavigationVolume[] volumes = FindObjectsOfType<NavigationVolume>();
+                for (int i = 0; i < volumes.Length; i++)
+                {
+                    volumes[i].gameObject.layer = LayerMask.NameToLayer(Manager.VolumeLayerName);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Layers are not generated yet, make sure to first generate them");
+            }
         }
     }
 }
---
+++ Packages/com.tecmaid.spatialpathfinding/Editor/TagFactory.cs
@@ -0,0 +1,216 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Pathfinding.Helpers
+{
+    public class TagFactory : Editor
+    {
+        public static bool CreateTag(string tagName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            // Tags Property
+            SerializedProperty tagsProp = tagManager.FindProperty("tags");
+            if (tagsProp.arraySize >= 31)
+            {
+                Debug.Log("No more tags can be added to the Tags property. You have " + tagsProp.arraySize + " tags");
+                return false;
+            }
+            // if not found, add it
+            if (!PropertyExists(tagsProp, 0, tagsProp.arraySize, tagName))
+            {
+                int index = tagsProp.arraySize;
+                // Insert new array element
+                tagsProp.InsertArrayElementAtIndex(index);
+                SerializedProperty sp = tagsProp.GetArrayElementAtIndex(index);
+                // Set array element to tagName
+                sp.stringValue = tagName;
+                Debug.Log("Tag: " + tagName + " has been added");
+                // Save settings
+                tagManager.ApplyModifiedProperties();
+
+                return true;
+            }
+            else
+            {
+                //Debug.Log ("Tag: " + tagName + " already exists");
+            }
+            return false;
+        }
+
+        public static string NewTag(string name)
+        {
+            CreateTag(name);
+
+            if (name == null || name == "")
+            {
+                name = "Untagged";
+            }
+
+            return name;
+        }
+
+        public static bool RemoveTag(string tagName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+            // Tags Property
+            SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+            if (PropertyExists(tagsProp, 0, tagsProp.arraySize, tagName))
+            {
+                SerializedProperty sp;
+
+                for (int i = 0, j = tagsProp.arraySize; i < j; i++)
+                {
+
+                    sp = tagsProp.GetArrayElementAtIndex(i);
+                    if (sp.stringValue == tagName)
+                    {
+                        tagsProp.DeleteArrayElementAtIndex(i);
+                        Debug.Log("Tag: " + tagName + " has been removed");
+                        // Save settings
+                        tagManager.ApplyModifiedProperties();
+                        return true;
+                    }
+
+                }
+            }
+
+            return false;
+        }
+
+        public static bool TagExists(string tagName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+            // Layers Property
+            SerializedProperty tagsProp = tagManager.FindProperty("tags");
+            return PropertyExists(tagsProp, 0, 31, tagName);
+        }
+
+        public static bool CreateLayer(string layerName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            // Layers Property
+            SerializedProperty layersProp = tagManager.FindProperty("layers");
+            if (!PropertyExists(layersProp, 0, 31, layerName))
+            {
+                SerializedProperty sp;
+                // Start at layer 9th index -> 8 (zero based) => first 8 reserved for unity / greyed out
+                for (int i = 8, j = 31; i < j; i++)
+                {
+                    sp = layersProp.GetArrayElementAtIndex(i);
+                    if (sp.stringValue == "")
+                    {
+                        // Assign string value to layer
+                        sp.stringValue = layerName;
+                        Debug.Log("Layer: " + layerName + " has been added");
+                        // Save settings
+                        tagManager.ApplyModifiedProperties();
+                        return true;
+                    }
+                    if (i == j)
+                        Debug.Log("All allowed layers have been filled");
+                }
+            }
+            else
+            {
+                //Debug.Log ("Layer: " + layerName + " already exists");
+            }
+            return false;
+        }
+
+        public static string NewLayer(string name)
+        {
+            if (name != null || name != "")
+            {
+                CreateLayer(name);
+            }
+
+            return name;
+        }
+
+        public static bool RemoveLayer(string layerName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+            // Tags Property
+            SerializedProperty layersProp = tagManager.FindProperty("layers");
+
+            if (PropertyExists(layersProp, 0, layersProp.arraySize, layerName))
+            {
+                SerializedProperty sp;
+
+                for (int i = 0, j = layersProp.arraySize; i < j; i++)
+                {
+
+                    sp = layersProp.GetArrayElementAtIndex(i);
+
+                    if (sp.stringValue == layerName)
+                    {
+                        sp.stringValue = "";
+                        Debug.Log("Layer: " + layerName + " has been removed");
+                        // Save settings
+                        tagManager.ApplyModifiedProperties();
+                        return true;
+                    }
+
+                }
+            }
+
+            return false;
+
+        }
+
+        public static bool LayerExists(string layerName)
+        {
+            // Open tag manager
+            SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+            // Layers Property
+            SerializedProperty layersProp = tagManager.FindProperty("layers");
+            return PropertyExists(layersProp, 0, 31, layerName);
+        }
+
+        private static bool PropertyExists(SerializedProperty property, int start, int end, string value)
+        {
+            for (int i = start; i < end; i++)
+            {
+                SerializedProperty t = property.GetArrayElementAtIndex(i);
+                if (t.stringValue.Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddNewTag(string name)
+        {
+            CreateTag(name);
+        }
+
+        public void DeleteTag(string name)
+        {
+            RemoveTag(name);
+        }
+
+        public void AddNewLayer(string name)
+        {
+            CreateLayer(name);
+        }
+
+        public void DeleteLayer(string name)
+        {
+            RemoveLayer(name);
+        }
+    }
+}
+
